1b285ddd280b7dc3ec790a00a3147e9bfe49fb2f
ulyana <ulyana.k.topdevs@gmail.com>
2021-07-09 17:56:08 +0300

    finished kvadratlonworld

--- kvadratlonworld.com/resources/assets/js/components/FormInput.vue
+++ kvadratlonworld.com/resources/assets/js/components/FormInput.vue
@@ -24,6 +24,18 @@
       @input="$emit('input', $event)"
       :mask="mask"
     />
+    <input
+        v-else-if="name === 'password'"
+        class="form-field__input"
+        :class="{
+        'form-field__input--reversed': reversed,
+        'form-field__input--has-error': error
+      }"
+        :placeholder="placeholder || title"
+        :value="value"
+        @input="$emit('input', $event.target.value)"
+        type="password"
+    >
 
     <input
       v-else
@@ -60,7 +72,8 @@ export default {
     mask: String,
     rows: String,
     error: [String, Boolean],
-    reversed: Boolean
+    reversed: Boolean,
+    name: String
   }
 }
 </script>
